6ddcd209cd02efb746d648d50072c44dd0e394c5
snegarj553 <snega.r@ionixxtech.com>
2022-07-13 11:38:44 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function App() {
-
   const storedetails = useSelector((data) => data);
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  return   dispatch({ type:"FETCH_USER"});
-  
-};
+    return dispatch({ type: 'FETCH_USER' });
+  };
 
   return (
-    <form>
-     
-
+    <div className="form">
       <button onClick={handleSubmit}>Fetch User</button>
 
-{console.log("data",storedetails)}
-     { storedetails.map(data=>
-   <>  {console.log("title",data.title)}
-      <p>{data.title} </p>
-      </>
-      )}
-    </form>
+      <table>
+        <thead>
+          <th>UserId</th>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Details </th>
+        </thead>
+<tbody>
+        {storedetails.map((data) => (
+          <tr key={data.userId}>
+            <td>{data.userId}</td>
+               <td>{data.id}</td>
+            <td>{data.title}</td>
+            <td>{data.body}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
